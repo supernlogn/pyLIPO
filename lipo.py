105678f05821e55ed5e71e527bb40045f69577d6
supernlogn <ath.ioannis94@gmail.com>
2018-06-04 03:35:14 +0300

    adalipo: support vector members to input, no sorted lipschitz constants array.
    The curse of dimensionality is very intense to this algorithm
    and no solutions close to exact are guaranteed.

--- lipo.py
+++ lipo.py
@@ -36,32 +36,35 @@ def lipo(n, k, X, f, MAX_ITERATIONS_PER_T = 100):
       it_per_t = 0
   return Xi[np.argmax(fx)]
 
+def _len(X):
+  return np.prod(np.shape(X))
+
 def adalipo(n, K, X, f, p):
   """
-    This the adalipo algorithm as described in section 4
+    This the adalipo algorithm described in section 4
     of the work of Malherbe and Vayatis. It does not require a specific k
     value as lipo, but a grid of values where to search for it.
     Args:
       n : number of iterations
-      K : grid of lipschitz constants (non-decreasing list or numpy array)
+      K : grid of lipschitz constants (list or numpy array)
       X : possible inputs of f
       f : the lipschitz function which to find maxima (as a function object)
       p : propability of exploitation vs (1-p) propability of exploration
     Returns:
-      The estimated value which maximizes f.
+        The estimated value which maximizes f.
   """
   # initialize I as a numpy array of input X
   # for faster computations
-  if(len(np.shape(X)) > 1):
-    I = np.squeeze(X)
-  else:
-    I = np.array(X)
+  # if(len(np.shape(X)) > 1):
+  #   I = np.squeeze(X)
+  # else:
+  I = np.array(X)
   if(len(I.shape) == 1):
     I = np.expand_dims(I, -1)
   # make sure that K will be a numpy array
-  Kg = np.array(K)
+  Kg = np.sort(np.array(K))
   # Initialization part of the algorithm
-  x1 = np.random.choice(X)
+  x1 = np.squeeze(I[np.random.randint(I.shape[0], size=1), :], axis=0)
   fx = np.zeros((n,)) # holds the functions values computed so far...
   fx[0] = f(x1)
   Xi = [x1]  # holds the functions input values explored or exploited so far...
@@ -72,44 +75,58 @@ def adalipo(n, K, X, f, p):
   while(t < n):
     Bnew = np.random.choice([0, 1], None, [p, 1-p])
     if(Bnew == 1): # exploration
-      Xnew = np.random.choice(X)
+      Xnew = np.squeeze(I[np.random.randint(I.shape[0], size=1), :])
     else: # exploitation
       mx = np.max(fx, axis=-1)
-      if(len(np.shape(X)) > 1):
-        Ii = np.squeeze(Xi)
-      else:
-        Ii = np.array(Xi)
+      # if(len(np.shape(I)) > 1):
+        # Ii = np.squeeze(Xi)
+      # else:
+      Ii = np.array(Xi)
       if(len(Ii.shape) == 1):
-        Ii = np.expand_dims(Ii, -1)
+        Ii = np.expand_dims(Ii, 0)
+      # print(Ii)
       # l contains the norm-2 distance between the points the algorithm
       # has explored and the points in the possible inputs of f
       l = np.zeros((len(Ii), len(I)))
       for idx_i, Iii in enumerate(Ii):
-        for idx_j, Ij in enumerate(I):
-          l[idx_i, idx_j] = np.linalg.norm(Iii - Ij)
+        s_outer = np.subtract.outer(np.expand_dims(Iii, 0), I)
+        d = [s_outer[0, i, :, i] for i in xrange(np.shape(I)[-1])]
+        l[idx_i,:] = np.linalg.norm(d, axis=0)
       # w is an estimation of the term inside the min inside 
       # the Lemma 8 after definition 7
       w = knew * l
       nfx = fx[:t]
-      for i in range(np.shape(w)[1]):
+      for i in xrange(np.shape(w)[1]):
         w[:,i] += nfx
-      Xkt = X[np.min(w, axis=0) >= mx]
-      if(len(Xkt) != 0): # exploit
-        Xnew = np.random.choice(Xkt)
+      Xkt = X[np.min(w, axis=0) >= mx, :]
+      if(_len(Xkt) != 0): # exploit
+        Xnew = np.squeeze(Xkt[np.random.randint(Xkt.shape[0], size=1), :])
       else: # explore again...
-        Xnew = np.random.choice(X)
+        Xnew = np.squeeze(I[np.random.randint(I.shape[0], size=1), :])
+    if(len(np.shape(Xnew)) < 1):
+      Xnew = np.expand_dims(Xnew, -1)
     Xi.append(Xnew)
     fx[t] = f(Xnew)
     df = np.subtract.outer(fx[:t+1], fx[:t+1])
-    fs = np.zeros(((len(Xi)) * (len(Xi) - 1), ))
+    fs = []
     idx = 0
-    for i, xi in enumerate(Xi):
-      for j, xj in enumerate(Xi):
-        if(xi != xj):
-          fs[idx] = np.linalg.norm(df[i][j]) / np.linalg.norm(xi-xj)
-          idx += 1
-    knew = np.min(Kg[Kg > max(fs)])
+    # fs = np.zeros(((len(Xi)) * (len(Xi) - 1), ))
+    ndf = np.linalg.norm(np.expand_dims(df, -1), axis=-1)
+    # print(np.shape(Xi), Xi)
+    dX = np.subtract.outer(Xi, Xi)
+    # print(np.shape(dX), np.shape(Xi))
+    dx = np.array([dX[:,i,:,i] for i in xrange(np.shape(dX)[-1])])
+    ndx = np.linalg.norm(dx, axis=0)
+    for i in xrange(np.shape(ndf)[0]):
+      fs.extend(ndf[i,:i] / ndx[i,:i])
+      if(i+1 != np.shape(ndf)):
+        fs.extend(ndf[i,i+1:] / ndx[i,i+1:])
+    try:
+      knew = np.min(Kg[Kg > np.max(fs)])
+    except:
+      print("function has greater lipschitz constants than those provided")
+      return ValueError
     k.append(knew)
     t += 1
   # Output part of the algorithm
-  return Xi[np.argmax(fx)]
+  return np.squeeze(Xi[np.argmax(fx)])
--- test.py
+++ test.py
@@ -4,9 +4,17 @@ import numpy as np
 def func(x):
     return 1 - (x-1)*(x-1)
 
+def func2(x):
+  return 1 -(x[0] - 2)*(x[0] - 2)*(x[1] - 1)*(x[1] - 1)
+
+
 if __name__ == "__main__":
     X = np.arange(-5, 5, 0.001)
     k = 7
     print(lipo(10, k, X, func))
     K = np.power((1+0.5), np.arange(1, 7, 0.01))
-    print(adalipo(20, K, X, func, 0.5))
+    print(adalipo(20, K, np.expand_dims(X, 1), func, 0.5))
+    # test 2D
+    X = np.reshape(np.stack(np.meshgrid(np.arange(-10,10,0.01), np.arange(-10,10,0.01)), axis=-1), [-1,2])
+    K = np.concatenate([np.arange(0.1,1.5, 0.01), np.power((1+0.5), np.arange(2.0, 49.0, 0.1))])
+    print(adalipo(20, K, X, func2, 0.5))
